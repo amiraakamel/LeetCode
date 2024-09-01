947b5116d42445ce59097a0cbb8a0027f5ac7d98
Amira Kamel <168797176+amiraakamel@users.noreply.github.com>
2024-09-02 01:43:00 +0300

    Time: 98 ms (8.79%), Space: 88 MB (73.82%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -6,10 +6,11 @@ public:
         }
         
         vector<vector<int>> ans(m, vector <int> (n));
-        
+        int index =0;
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<n ; j++){
-                ans[i][j] = original[i*n+j];
+                ans[i][j] = original[index];
+                index++;
             }
         }
         return ans;
